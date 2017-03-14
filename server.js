df46b64d45d4440b47919c68297714f1638ef85d
Jose Balado <jose.balado@gmail.com>
2017-03-14 22:35:33 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+app.use(express.static('public'));
+
+
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+        console.log('OPTIONS was requested');
+        let headers = {};
+        headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, accept, authorization";
+        headers["Access-Control-Allow-Methods"] = "GET";
+        headers["Access-Control-Allow-Origin"] = "*";
+        res.writeHead(200, headers);
+        res.end();
+  }
+  else {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  }
+});
+
+app.get('/logon', (req, res) => {
+  console.log('logon was called');
+  fs.createReadStream('logon.json').pipe(res);
+});
+
+
+// cITm locations service
+app.get('/Monitoring/MasterDataService.svc/Locations', (req, res) => {
+  console.log('cITm locations was called');
+  fs.createReadStream('cITm-locations.json').pipe(res);
+});
+
+// cITm sample workload data service
+app.get('/Monitoring/MonitoringService.svc/sample_count', (req, res) => {
+  console.log('cITm sample_count was called');
+  fs.createReadStream('cITm-sample_count.json').pipe(res);
+});
+
+
+// Infinity workareas service
+app.get('/csp/acb/services/Monitoring/MasterDataService.svc/Workareas', (req, res) => {
+  console.log('Infinity workareas was called');
+  fs.createReadStream('infinity-workareas.json').pipe(res);
+});
+
+// Infinity sample workload data service
+app.get('/csp/acb/services/Monitoring/MonitoringService.svc/inf_sample_count', (req, res) => {
+  console.log('Infinity sample workload data was called');
+  fs.createReadStream('infinity-sample-workload-data.json').pipe(res);
+});
+
+
+app.get('*', function (req, res) {
+  res.status(404).send('Error 404: Resource not found');
+});
+
+app.post('*', function (req, res) {
+  console.log(req);
+});
+
+app.listen(3000, () => 
+    console.log('server running on: http://127.0.0.1:3000')
+);
+

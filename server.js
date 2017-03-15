0f7e0050446c173a73211822bcb3c197c3e785b3
Jose Balado <jose.balado@contractors.roche.com>
2017-03-15 17:50:50 +0100

    Added cTAT segmented response

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.use((req, res, next) => {
         headers["Access-Control-Allow-Origin"] = "*";
         res.writeHead(200, headers);
         res.end();
-  }
-  else {
+  } else {
     res.header("Access-Control-Allow-Origin", "*");
     next();
   }
@@ -53,6 +52,22 @@ app.get('/csp/acb/services/Monitoring/MonitoringService.svc/inf_sample_count', (
   fs.createReadStream('infinity-sample-workload-data.json').pipe(res);
 });
 
+
+// Current TAT overview segmented workareas service
+app.get(/inf_ovvtarget/, (req, res, next) => {
+  if (req.query.workarea === '20') {
+    console.log('Current TAT segmented workarea 20 was called');
+    fs.createReadStream('current_tat-workareas-segmented.json').pipe(res);
+  } else {
+    next();
+  }
+});
+
+// Current TAT workload data service
+app.get('/*inf_ovvtarget', (req, res) => {
+  console.log('Current TAT detail view data was called');
+  fs.createReadStream('current_tat-only-view-data.json').pipe(res);
+});
 
 app.get('*', function (req, res) {
   res.status(404).send('Error 404: Resource not found');

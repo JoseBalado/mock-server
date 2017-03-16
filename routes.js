6942a89998df6934be1c668520fe0f4f4f051408
Jose Balado <jose.balado@contractors.roche.com>
2017-03-16 12:36:35 +0100

    Added app.Router to handle routes

---
+++ routes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+router.get('/logon', (req, res) => {
+  console.log('logon was called');
+  fs.createReadStream('logon.json').pipe(res);
+});
+
+
+// cITm locations service
+router.get('/Monitoring/MasterDataService.svc/Locations', (req, res) => {
+  console.log('cITm locations was called');
+  fs.createReadStream('data/cITm-locations.json').pipe(res);
+});
+
+// cITm sample workload data service
+router.get('/Monitoring/MonitoringService.svc/sample_count', (req, res) => {
+  console.log('cITm sample_count was called');
+  fs.createReadStream('data/cITm-sample_count.json').pipe(res);
+});
+
+
+// Infinity workareas service
+router.get('/csp/acb/services/Monitoring/MasterDataService.svc/Workareas', (req, res) => {
+  console.log('Infinity workareas was called');
+  fs.createReadStream('data/infinity-workareas.json').pipe(res);
+});
+
+// Infinity sample workload data service
+router.get('/csp/acb/services/Monitoring/MonitoringService.svc/inf_sample_count', (req, res) => {
+  console.log('Infinity sample workload data was called');
+  fs.createReadStream('data/infinity-sample-workload-data.json').pipe(res);
+});
+
+
+// Current TAT overview segmented workareas service
+router.get(/inf_ovvtarget/, (req, res, next) => {
+  if (req.query.workarea === '20') {
+    console.log('Current TAT segmented workarea 20 was called');
+    fs.createReadStream('data/current_tat-workareas-segmented.json').pipe(res);
+  } else {
+    next();
+  }
+});
+
+// Current TAT workload data service
+router.get('/*inf_ovvtarget', (req, res) => {
+  console.log('Current TAT detail view data was called');
+  fs.createReadStream('data/current_tat-only-view-data.json').pipe(res);
+});
+
+router.get('*', function (req, res) {
+  res.status(404).send('Error 404: Resource not found');
+});
+
+router.post('*', function (req, res) {
+  console.log(req);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-
+const router = require('./routes');
 
 app.use(express.static('public'));
-
 
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
@@ -21,61 +19,7 @@ app.use((req, res, next) => {
   }
 });
 
-app.get('/logon', (req, res) => {
-  console.log('logon was called');
-  fs.createReadStream('logon.json').pipe(res);
-});
-
-
-// cITm locations service
-app.get('/Monitoring/MasterDataService.svc/Locations', (req, res) => {
-  console.log('cITm locations was called');
-  fs.createReadStream('data/cITm-locations.json').pipe(res);
-});
-
-// cITm sample workload data service
-app.get('/Monitoring/MonitoringService.svc/sample_count', (req, res) => {
-  console.log('cITm sample_count was called');
-  fs.createReadStream('data/cITm-sample_count.json').pipe(res);
-});
-
-
-// Infinity workareas service
-app.get('/csp/acb/services/Monitoring/MasterDataService.svc/Workareas', (req, res) => {
-  console.log('Infinity workareas was called');
-  fs.createReadStream('data/infinity-workareas.json').pipe(res);
-});
-
-// Infinity sample workload data service
-app.get('/csp/acb/services/Monitoring/MonitoringService.svc/inf_sample_count', (req, res) => {
-  console.log('Infinity sample workload data was called');
-  fs.createReadStream('data/infinity-sample-workload-data.json').pipe(res);
-});
-
-
-// Current TAT overview segmented workareas service
-app.get(/inf_ovvtarget/, (req, res, next) => {
-  if (req.query.workarea === '20') {
-    console.log('Current TAT segmented workarea 20 was called');
-    fs.createReadStream('data/current_tat-workareas-segmented.json').pipe(res);
-  } else {
-    next();
-  }
-});
-
-// Current TAT workload data service
-app.get('/*inf_ovvtarget', (req, res) => {
-  console.log('Current TAT detail view data was called');
-  fs.createReadStream('data/current_tat-only-view-data.json').pipe(res);
-});
-
-app.get('*', function (req, res) {
-  res.status(404).send('Error 404: Resource not found');
-});
-
-app.post('*', function (req, res) {
-  console.log(req);
-});
+app.use('/', router);
 
 app.listen(3000, () => 
     console.log('server running on: http://127.0.0.1:3000')
